ea9144b1bb7a2aece77cb7b691b4314fbc961a95
Mikkareem <mohdirsath1997@gmail.com>
2024-08-01 19:43:46 +0530

    starter

---
+++ app/src/main/java/com/example/openglshadersdemo/opengl/OpenGLSurfaceViewRenderer.kt
@@ -0,0 +1,40 @@
+package com.example.openglshadersdemo.opengl
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView.Renderer
+import android.opengl.Matrix
+import android.os.SystemClock
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class OpenGLSurfaceViewRenderer : Renderer {
+    private val vPMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
+    private val rotationMatrix = FloatArray(16)
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES20.glClearColor(0.4334f, 0f, 0f, 1f)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+        val aspect = width.toFloat() / height.toFloat()
+        Matrix.frustumM(projectionMatrix, 0, -aspect, aspect, -1f, 1f, 3f, 7f)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+        val scratch = FloatArray(16)
+
+        val time = SystemClock.uptimeMillis() % 4000L
+        val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(rotationMatrix, 0, angle, 1f, 1f, 0f)
+
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, 6f, 0f, 0f, 0f, 0f, 1f, 0f)
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0)
+    }
+}
